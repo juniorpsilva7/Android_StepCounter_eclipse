cecc5441276ec57ada3536eccc345d25e05d44e4
Junior Silva <juniorpsilva7@gmail.com>
2016-10-17 22:33:07 -0200

    Add some comments in portuguese

--- src/com/contadorpassos/MainActivity.java
+++ src/com/contadorpassos/MainActivity.java
@@ -1,9 +1,8 @@
 package com.contadorpassos;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.res.Configuration;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -15,14 +14,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements SensorEventListener, OnClickListener{
+
+public class MainActivity extends Activity implements SensorEventListener{
 
 	private int mStepValue;
-	private final static int SENSITIVITY = 13;
+	private int StepAux;
+	private final static int SENSITIVITY = 12;
 	
 	private boolean mInitialized;
 	private SensorManager mSensorManager;
 	private Sensor mAccelerometer;
+	
+	//variavel booleana para deixar o serviço do acelerometro só iniciar quando clicar em START
+	public boolean proginiciado = false;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,16 +34,28 @@ public class MainActivity extends Activity implements SensorEventListener, OnCli
 		mStepValue = 0;
 		setContentView(R.layout.activity_main);
 		mInitialized = false;
+		
+		AlertDialog.Builder msg1 = new AlertDialog.Builder(MainActivity.this);
+		msg1.setTitle("Bluetooth");
+		msg1.setMessage("Neste momento seria conectado o Bluetooth com o Micro e ele iria converter para serial do Arduino");
+		msg1.setNeutralButton("Ok", null);
+		msg1.show();
+		
+		
+		
+		//Pega a instância do sensor de aceleração padrão
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 		
+				
 		final Button startbutton = (Button) findViewById(R.id.btnStart);
         startbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Perform action on click
+                // ação ao clicar
+            	proginiciado = true; // seta o booleano informando que iniciou a aplicação
             	TextView numberSteps = (TextView)findViewById( R.id.txtCount);
             	numberSteps.setText(Integer.toString(0));
+            	//na função OnResume() ele inicia a instância do sensor acelerômetro
             	onResume();
             }
         });
@@ -47,9 +63,10 @@ public class MainActivity extends Activity implements SensorEventListener, OnCli
         final Button stopbutton = (Button) findViewById(R.id.btnStop);
         stopbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Perform action on click
+                // ação ao clicar
             	TextView numberSteps = (TextView)findViewById( R.id.txtCount);
             	numberSteps.setText(Integer.toString(0));
+            	//na função OnPause() ele pausa a instância do sensor acelerômetro
             	onPause();
             }
         });
@@ -66,7 +83,12 @@ public class MainActivity extends Activity implements SensorEventListener, OnCli
 
 	protected void onResume() {
 		super.onResume();
-		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+		// se o botão START foi clicado
+		if(proginiciado){
+			mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+		}else{
+			onPause();
+		}
 	}
 
 	protected void onPause() {
@@ -83,9 +105,9 @@ public class MainActivity extends Activity implements SensorEventListener, OnCli
 
 	@Override
 	public void onSensorChanged(SensorEvent event){
-		// alpha is calculated as t / (t + dT)
-		// with t, the low-pass filter's time-constant
-		// and dT, the event delivery rate
+		// alpha é calculado como t / (t + dT)
+		// com t sendo a constante tempo do filtro low-pass
+		// e dT sendo a taxa de entrega do evento
 		
 		TextView numberSteps = (TextView)findViewById( R.id.txtCount);
 
@@ -98,20 +120,34 @@ public class MainActivity extends Activity implements SensorEventListener, OnCli
 
 			final float alpha = (float) 0.8;
 
+			// isola a força da gravidade  com o filtro low-pass
+			// em cada eixo
 			gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
 			gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
 			gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-
+			
+			// remove a contribuição da gravidade com o filtro high-pass
+			// em cada eixo
 			linear_acceleration[0] = event.values[0] - gravity[0];
 			linear_acceleration[1] = event.values[1] - gravity[1];
 			linear_acceleration[2] = event.values[2] - gravity[2];
-			System.out.println(linear_acceleration[0]+" , "+ linear_acceleration[1]+" , "+linear_acceleration[2]+" , ");
+			
 
 			mStepValue = Integer.parseInt((String) numberSteps.getText());
-			if(/*linear_acceleration[0] > SENSITIVITY || linear_acceleration[1] > SENSITIVITY ||*/ linear_acceleration[2] > SENSITIVITY && linear_acceleration[2]>0){
-				System.out.println("ONEEEE STEPP");
+			// incrementa a variavel que conta a quantidade de passos se
+			// as acelerações lineares em todos os eixos forem maior que
+			// a variavel SENSIVITY e todas juntas forem maior que zero
+			if(linear_acceleration[0] > SENSITIVITY || linear_acceleration[1] > SENSITIVITY || linear_acceleration[2] > SENSITIVITY && linear_acceleration[2]>0){
 				numberSteps.setText(Integer.toString(++mStepValue));
-				System.out.println(numberSteps);
+				StepAux++;
+				if (StepAux == 20){
+					AlertDialog.Builder msg2 = new AlertDialog.Builder(MainActivity.this);
+					msg2.setTitle("Relatorio");
+					msg2.setMessage("Neste momento seria enviado um relatório para alguma unidade de armazenamento para fins de relatórios médicos.");
+					msg2.setNeutralButton("Ok", null);
+					msg2.show();
+					StepAux = 0;
+				}
 			}
 		}
 	}
@@ -120,13 +156,6 @@ public class MainActivity extends Activity implements SensorEventListener, OnCli
 	public void onAccuracyChanged(Sensor sensor, int accuracy){
 
 	}
-
-	@Override
-	public void onClick(DialogInterface arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 
 
 }
